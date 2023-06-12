51a2c1679e4426631361b87b772313a4643d5469
Bernd Dammer <Bernd.Dammer@web.de>
2023-06-12 20:33:48 +0200

    P16Blink

---
+++ src/task_blinker.h
@@ -0,0 +1,15 @@
+/*
+ * task_blinker.h
+ *
+ *  Created on: 12.06.2023
+ *      Author: manni4
+ */
+
+#ifndef TASK_BLINKER_H_
+#define TASK_BLINKER_H_
+
+
+void blinker_init();
+
+
+#endif /* TASK_BLINKER_H_ */
---
+++ src/task_prio.h
@@ -0,0 +1,17 @@
+/*
+ * task_prio.h
+ *
+ *  Created on: 12.06.2023
+ *      Author: manni4
+ */
+
+#ifndef TASK_PRIO_H_
+#define TASK_PRIO_H_
+
+
+#define BLINKER_TASK_PRIO (tskIDLE_PRIORITY + 1)
+
+
+
+
+#endif /* TASK_PRIO_H_ */
