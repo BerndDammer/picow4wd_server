a7722d6d90a0da9c8f29e8fdb64fbcbe6d5c9448
Bernd Dammer <Bernd.Dammer@web.de>
2023-06-23 09:00:09 +0200

    drive control

--- src/task_drive.c
+++ src/task_drive.c
@@ -5,25 +5,50 @@
  *      Author: manni4
  */
 
+#include <stdio.h>
+
 #include "task_drive.h"
 #include "task_prio.h"
 #include "can.h"
+#include "hardware_drive.h"
 
 static TaskHandle_t drive_taskhandle;
 
+#define POWER_OFF_DELAY_TICK pdMS_TO_TICKS(1000)
+
 void drive_thread(MainEnvironement_t *MainEnvironement)
 {
+	motor_init();
+
 	while (true)
 	{
 		can_msg_t msg;
 		BaseType_t result;
 
-		result = xQueueReceive(MainEnvironement->to_drive, &msg, XDELAY);
+		result = xQueueReceive(MainEnvironement->to_drive, &msg,
+				POWER_OFF_DELAY_TICK);
 		if (result == pdPASS)
 		{
+			switch (msg.id)
+			{
+			case CAN___ID_DRIVE:
+				motor_set(FRONT_LEFT, *(signed short*) &msg.data[0]);
+				motor_set(FRONT_RIGHT, *(signed short*) &msg.data[2]);
+				motor_set(REAR_LEFT, *(signed short*) &msg.data[4]);
+				motor_set(REAR_RIGHT, *(signed short*) &msg.data[6]);
+				break;
+			default:
+				puts("You should not be here");
+				break;
+			}
 		}
 		else if (result == errQUEUE_EMPTY)
 		{
+			// control timeout switches engines off
+			motor_set(FRONT_LEFT, 0);
+			motor_set(FRONT_RIGHT, 0);
+			motor_set(REAR_LEFT, 0);
+			motor_set(REAR_RIGHT, 0);
 		}
 	}
 }
